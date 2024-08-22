84ea571a8ecda94de37353ceb44e302111517edf
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-08-22 12:23:21 -0500

    Add class todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,7 @@
-# TODO: Add code here
+class todo:
+    def __init__(self, code_id: int, title: str, description: str, completed: bool):
+            self.code_id = code_id
+            self.title = title
+            self.description = description
+            self.completed = False
+            self.tags = []
