31c362c9e5fbea6f3e7f67a62308c844127b60d0
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-08-22 13:28:40 -0500

    Finish the code

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,46 +1,46 @@
-from typing import Dict, List
+from typing import List, Dict
 
-class todo:
-    def __init__(self, code_id: int, title: str, description: str, completed: bool):
-            self.code_id = code_id
-            self.title = title
-            self.description = description
-            self.completed = False
-            self.tags = []
+class Todo:
+    def __init__(self, code_id: int, title: str, description: str) -> None:
+        self.code_id = code_id
+        self.title = title
+        self.description = description
+        self.completed = False
+        self.tags = []
 
-    def mark_completed(self):
+    def mark_completed(self) -> None:
         self.completed = True
 
-    def add_tag(self, tag: str):
+    def add_tag(self, tag: str) -> None:
         if tag not in self.tags:
             self.tags.append(tag)
-    
-    def __str__ (self) -> str:
-        return f"{self.code_id} - {self.title}"
-    
-class TodoBook:
-     def __init__(self):
-        self.todos = Dict[int, todo] = {}
 
-     def add_todo(self, title: str, description: str) -> int:
-        code_id = len(self.todos) + 1
-        self.todos[code_id] = todo(code_id, title, description, False)
-        return code_id
-     
-     def pending_todos (self) -> List[todo]:
+    def __str__(self) -> str:
+        return f"{self.code_id} - {self.title}"
+
+
+class TodoBook:
+    def __init__(self) -> None:
+        self.todos: Dict[int, Todo] = {}
+
+    def add_todo(self, title: str, description: str) -> int:
+        new_id = len(self.todos) + 1
+        todo = Todo(new_id, title, description)
+        self.todos[new_id] = todo
+        return new_id
+
+    def pending_todos(self) -> List[Todo]:
         return [todo for todo in self.todos.values() if not todo.completed]
-     
-     def completed_todos (self) -> List[todo]:
+
+    def completed_todos(self) -> List[Todo]:
         return [todo for todo in self.todos.values() if todo.completed]
-     
-     def tags_todo_count(self) -> Dict[str, int]:
-        tags_count = {}
+
+    def tags_todo_count(self) -> Dict[str, int]:
+        tag_count = {}
         for todo in self.todos.values():
             for tag in todo.tags:
-                if tag in tags_count:
-                    tags_count[tag] += 1
+                if tag in tag_count:
+                    tag_count[tag] += 1
                 else:
-                    tags_count[tag] = 1
-        return tags_count
-     
-        
+                    tag_count[tag] = 1
+        return tag_count
