e53031397a236bf96c9e5aea830ef601b8f3b48b
Andres Ortega <aortega473@soyudemedellin.edu.co>
2024-08-22 12:32:20 -0500

    Finish class todo

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -5,3 +5,16 @@ class todo:
             self.description = description
             self.completed = False
             self.tags = []
+
+    def mark_completed(self):
+        self.completed = True
+
+    def add_tag(self, tag: str):
+        if tag not in self.tags:
+            self.tags.append(tag)
+    
+    def __str__ (self) -> str:
+        return f"{self.code_id} - {self.title}"
+    
+
+    
